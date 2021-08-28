8917e41764d04a14501e0fe9872226cff2583205
nahee-park <jessica396@ewhain.net>
2021-08-28 15:16:07 +0900

    feat: Join Room 퍼블리싱

---
+++ self_chat_filesystem/client/src/component/JoinModal.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Input,
+} from "reactstrap";
+
+function Join({ modal, toggle }) {
+  return (
+    <div>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalBody>
+          <Input placeholder="채팅방 이름을 입력하세요" rows={5} />
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>
+            Join
+          </Button>{" "}
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
+
+export default Join;
