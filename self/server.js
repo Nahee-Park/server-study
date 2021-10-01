f5f3d92e02b2e05f718731d91d4dff7fbfcfd18c
nahee-park <jessica396@ewhain.net>
2021-10-01 17:59:15 +0900

    feat: 로그인 페이지 만들기, 아이디 비번 검사해 세션 만들기

--- self/server.js
+++ self/server.js
@@ -1,4 +1,9 @@
 const express = require("express");
+// 로그인, 세션 생성을 위한 라이브러리(passport, passport-local express-session->실제 서비스에선 이거 말고 몽고디비에 세션데이터 저장해주는 라이브러리 이용)
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+
 // 라이브러리를 바탕으로 객체 설정
 const app = express();
 let db;
@@ -24,6 +29,14 @@ MongoClient.connect(
 
 // req 풀기 위한 코드 (미들웨어가 요청 받은 것들을 풀어줘야 서버에서 해석 가능)
 app.use(express.urlencoded({ extended: true }));
+
+// 서버의 요청과 응답 사이에 실행할 코드
+// 어떤 사람이 로그인 -> 그 사람의 아이디와 비번이 DB에 있는 것과 같은 지 검사 -> 검사 결과가 맞으면 세션 하나 생성해주고 성공 페이지로 이동시키기, 실패하면 실패 페이지로 이동시키기
+app.use(
+  session({ secret: "비밀코드", resave: true, saveUninitialized: false })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // .sefFile을 통해 파일을 보냄
 app.get("/", (req, res) => {
@@ -118,4 +131,60 @@ app.get("/detail/:id", (req, res) => {
       res.render("detail.ejs", { data: result });
     }
   );
+});
+
+//사실상 지금 라우팅을 서버 측에서 다 하고 있는 것은
+//login페이지 접속 시 login.ejs 페이지 보여주도록
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+// login경로로 post시 /페이지로 리다이렉트 되도록
+app.post("/login", (req, res) => {
+  res.redirect("/");
+});
+
+// 어떻게 인증할 건지에 대한 세부 코드
+passport.use(
+  // LocalStrategy() -> local방식으로 아이디, 비번 겁사 어떻게 할 지 도와주는 부분
+  new LocalStrategy(
+    //여기가 설정하는 부분
+    {
+      usernameField: "name", //사용자가 제출한 아이디가 어디 적혔는지
+      passwordField: "pw", //사용자가 제출한 비번이 어디 적혔는지
+      session: true, //세션 만들 건지
+      passReqToCallback: false, //아이디, 비번 말고 다른 정보 검사가 필요한지
+    },
+    // 아이디 비번 검사하는 코드
+    function (inputName, inputPw, done) {
+      console.log(입력한아이디, 입력한비번);
+      // 일단 아이디를 키로 해서 찾기 때문에 없으면 에러를 보냄
+      db.collection("login").findOne(
+        { name: inputName },
+        function (err, result) {
+          if (err) return done(err);
+          if (!result)
+            return done(null, false, {
+              message: "존재하지않는 아이디 입니다.",
+            });
+          // 실제 서비스에서는 암호화하는 과정 필요
+          if (inputPw == result.pw) {
+            return done(null, result);
+          } else {
+            return done(null, false, { message: "비번 틀렸어요" });
+          }
+        }
+      );
+    }
+  )
+);
+// 아이디 비번이 맞으면 1.세션 데이터 만들기 2. 그 안에 포함된 세션 아이디 발급하기 3. 유저한테 보내주기
+
+//유저의 아이디 데이터를 바탕으로 세션 데이터를 만들어 줌 -> 그 세션 데이터의 아이디를 쿠키로 만들어서 사용자의 브라우저로 보내줌
+passport.serializeUser(function (user, done) {
+  done(null, user.name);
+});
+
+passport.deserializeUser(function (name, done) {
+  done(null, {});
 });
